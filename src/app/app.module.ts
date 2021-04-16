fdcfad1adc309efda8da7d5e2db102bbbfc936ab
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2021-04-15 18:37:06 -0700

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,29 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SigninComponent } from './pages/signin/signin.component';
+import { UserCreateComponent } from './pages/user-create/user-create.component';
+import { UserDetailsComponent } from './pages/user-details/user-details.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { SecurityQuestionCreateComponent } from './pages/security-question-create/security-question-create.component';
+import { SecurityQuestionDetailsComponent } from './pages/security-question-details/security-question-details.component';
+import { SecurityQuestionListComponent } from './pages/security-question-list/security-question-list.component';
+import { DeleteRecordDialogComponent } from './shared/delete-record-dialog/delete-record-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BaseLayoutComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    SigninComponent,
+    UserCreateComponent,
+    UserDetailsComponent,
+    UserListComponent,
+    SecurityQuestionCreateComponent,
+    SecurityQuestionDetailsComponent,
+    SecurityQuestionListComponent,
+    DeleteRecordDialogComponent
   ],
   imports: [
     BrowserModule,
